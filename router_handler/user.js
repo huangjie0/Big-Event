8ad05c4e9fca0846ef9c451c55e66ac0eec5c893
huangjie <1364045672@qq.com>
2022-03-24 13:34:57 +0800

    优化

--- router_handler/user.js
+++ router_handler/user.js
@@ -1,5 +1,11 @@
 //注册新用户处理函数
 exports.regUser=(req,res)=>{
+    //获取客户端提交服务器信息
+    const userinfo = req.body;
+    //对表单数据进行合法校验
+    if(!userinfo.username||!userinfo.password){
+        return res.send({status:1,message:'用户名或者密码不合法'})
+    }
     res.send('reguser ok');
 }
 //注册登录处理函数
