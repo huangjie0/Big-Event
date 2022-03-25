19e2920317ffe4167c530d67cacbf3336b22a0c5
huangjie <1364045672@qq.com>
2022-03-25 08:59:17 +0800

    优化

--- app.js
+++ app.js
@@ -2,21 +2,23 @@
 const express = require('express');
 //创建服务器对象
 const app = express();
-const joi = require('@hapi/joi');
 //导入并配置cors中间件
 const cors = require('cors');
 app.use(cors());
-//定义错误级别的中间件
-app.use((err,req,res,next)=>{
-    //验证失败导致的错误
-    if(err instanceof joi.ValidationError){
-        return res.send({status:1,message:err.message});
-    }
-    res.send({status:1,message:'未知的错误'});
-    // next();
-})
 //配置解析表单数据中间件,解析application/x-www-form-urlencoded格式的表单数据
 app.use(express.urlencoded({extended:false}));
+//封装res.cc函数
+app.use((req,res,next)=>{
+    //status默认值为1，表示失败的情况
+    //err可能是一个错误对象也可能是一个错误描述字符串
+    res.cc=function(err,status=1){
+        res.send({
+            status,
+            message:err instanceof Error ? err.message : err,
+        })
+    }
+    next();
+})
 //导入路由模块
 const userRouter = require('./router/user');
 app.use('/api',userRouter);
--- router_handler/user.js
+++ router_handler/user.js
@@ -15,12 +15,13 @@ exports.regUser=(req,res)=>{
     db.query(sql,userinfo.username,(err,result)=>{
         //执行sql语句失败
         if(err){
-            return res.send({status:1,message:err.message})
+            // return res.send({status:1,message:err.message})
+            return res.cc(err)
         }
         //判断用户名是否被占用
         if(result.length>0){
-             res.send({status:1,message:'用户名被占用，请更换其他用户名'});
-             return;
+            //  res.send({status:1,message:'用户名被占用，请更换其他用户名'});
+             return res.cc('用户名被占用，请更换其他用户名')
         }
         //调用方法对密码进行加密
         userinfo.password = bcrypt.hashSync(userinfo.password,10);
@@ -28,14 +29,18 @@ exports.regUser=(req,res)=>{
         const sql = `insert into ev_users set ?`
         //调用db.query
         db.query(sql,{username:userinfo.username,password:userinfo.password},(err,result)=>{
+            // if(err){
+            //     return res.send({status:1,message:err.message})
+            // }
             if(err){
-                return res.send({status:1,message:err.message})
-            }
+                    return res.cc(err);
+                }
             //判断影响行数是否唯一
             if(result.affectedRows!=1){
-                res.send({status:1,message:'注册文本失败，请稍后再试'});
+                return res.cc('注册用户失效，请稍后再试！')
             }
-            res.send({status:0,message:'注册成功！'});
+            // res.send({status:0,message:'注册成功！'});
+            res.cc('注册成功！',0);
         }) 
     })
 }
