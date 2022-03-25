c9f8e289732f8eb542052f21ed21dfed9c421215
huangjie <1364045672@qq.com>
2022-03-25 10:59:15 +0800

    优化

--- router/user.js
+++ router/user.js
@@ -11,5 +11,5 @@ const {reg_login_schema} = require('../schema/user');
 //注册新用户的
 router.post('/reguser',expressJoi(reg_login_schema),user_handler.regUser);
 //登录的路由
-router.post('/login',user_handler.login);
+router.post('/login',expressJoi(reg_login_schema),user_handler.login);
 module.exports=router
